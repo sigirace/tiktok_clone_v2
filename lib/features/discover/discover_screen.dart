ea8316854949d795af47826d76d94febc173f861
SIGIRACE <chickang9@gmail.com>
2025-02-07 19:44:51 +0900

    feat: grid

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants/gaps.dart';
@@ -13,16 +14,44 @@ final List<String> _tabs = [
   "Brands",
 ];
 
-class DiscoverScreen extends StatelessWidget {
+class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
+
+  @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: _tabs.length,
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: const Text("Discover"),
+          title: CupertinoSearchTextField(
+            controller: _textController,
+            placeholder: "Search",
+            onChanged: (value) {
+              print(value);
+            },
+            onSubmitted: (value) {
+              print(value);
+            },
+          ),
           bottom: TabBar(
             splashFactory: NoSplash.splashFactory,
             padding: const EdgeInsets.symmetric(
@@ -46,6 +75,7 @@ class DiscoverScreen extends StatelessWidget {
         body: TabBarView(
           children: [
             GridView.builder(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
               itemCount: 10,
               padding: const EdgeInsets.all(Sizes.size8),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -56,14 +86,20 @@ class DiscoverScreen extends StatelessWidget {
               ),
               itemBuilder: (context, index) => Column(
                 children: [
-                  AspectRatio(
-                    aspectRatio: 9 / 16,
-                    child: FadeInImage.assetNetwork(
-                      placeholder: "assets/images/spider_wallpaper_1.jpeg",
-                      image:
-                          'https://i.pinimg.com/736x/64/cf/e8/64cfe81e4d0c27af546d5edcd6c15494.jpg',
-                      fit: BoxFit.cover,
-                      placeholderFit: BoxFit.cover,
+                  Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Sizes.size10),
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: FadeInImage.assetNetwork(
+                        placeholder: "assets/images/spider_wallpaper_1.jpeg",
+                        image:
+                            'https://i.pinimg.com/736x/64/cf/e8/64cfe81e4d0c27af546d5edcd6c15494.jpg',
+                        fit: BoxFit.cover,
+                        placeholderFit: BoxFit.cover,
+                      ),
                     ),
                   ),
                   Gaps.v8,
@@ -71,6 +107,7 @@ class DiscoverScreen extends StatelessWidget {
                     "This is a very long text that will be truncated, This is a very long text that will be truncated",
                     style: TextStyle(
                       fontSize: Sizes.size14,
+                      fontWeight: FontWeight.bold,
                     ),
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
