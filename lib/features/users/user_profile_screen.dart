dc5219f375ddcaafebcfbab6d63176daffe6576d
SIGIRACE <chickang9@gmail.com>
2025-02-11 05:14:48 +0900

    feat: settings screen

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone_v2/features/settings/settings_screen.dart';
 import 'package:tiktok_clone_v2/features/users/widgets/persistent_tab_bar.dart';
 
 import '../../constants/gaps.dart';
@@ -13,6 +14,13 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
+  void _onPressedSettings() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SettingsScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -25,7 +33,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 title: const Text('니꼬'),
                 actions: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: _onPressedSettings,
                     icon: const FaIcon(
                       FontAwesomeIcons.gear,
                       size: Sizes.size20,
